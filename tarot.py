9a100e4db9b559f248fb8cad4c0fceb11d4e03a7
Luke <lbvido22@gmail.com>
2026-03-19 13:39:36 +0800

    Update tarot cards list with major arcana

--- tarot.py
+++ tarot.py
@@ -1,6 +1,11 @@
 import random
- 
-cards = ["The Fool 0", "The Magician 1", "The High Priestess 2", "insert"]
+
+#major arcana 
+cards = ["The Fool (0)", "The Magician (I)", "The High Priestess (II)", "The Empress (III)", "The Emperor (IV),
+         "The Hierophant (V)", "The Lovers (VI)", "The Chariot (VII)", "Strength (VIII)", "The Hermit (IX)",
+         "The Wheel of Fortune (X)", "Justice (XI)", "The Hanged Man (XII)", "Death (XIII)", "Temperance (XIV)",
+         "The Devil (XV)", "The Tower (XVI)", "The Star (XVII)", "The Moon (XVIII)", "The Sun (XIX)", 
+         "Judgment (XX)", "The World (XXI)"] 
 
 #draw some cards
 reading = random.sample(cards, 3)
