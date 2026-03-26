72f3ff5007bbb9aa4a6de334f889f740bc5d9cc5
Luke <23814635@student.uwa.edu.au>
2026-03-26 12:25:20 +0800

    Fixed bug regarding Tarot Card list

--- tarot.py
+++ tarot.py
@@ -1,11 +1,29 @@
 import random
 
 #major arcana 
-cards = ["The Fool (0)", "The Magician (I)", "The High Priestess (II)", "The Empress (III)", "The Emperor (IV),
-         "The Hierophant (V)", "The Lovers (VI)", "The Chariot (VII)", "Strength (VIII)", "The Hermit (IX)",
-         "The Wheel of Fortune (X)", "Justice (XI)", "The Hanged Man (XII)", "Death (XIII)", "Temperance (XIV)",
-         "The Devil (XV)", "The Tower (XVI)", "The Star (XVII)", "The Moon (XVIII)", "The Sun (XIX)", 
-         "Judgment (XX)", "The World (XXI)"] 
+cards = ["The Fool (0)", 
+         "The Magician (I)", 
+         "The High Priestess (II)", 
+         "The Empress (III)", 
+         "The Emperor (IV)",
+         "The Hierophant (V)", 
+         "The Lovers (VI)", 
+         "The Chariot (VII)", 
+         "Strength (VIII)", 
+         "The Hermit (IX)",
+         "The Wheel of Fortune (X)", 
+         "Justice (XI)", 
+         "The Hanged Man (XII)", 
+         "Death (XIII)", 
+         "Temperance (XIV)",
+         "The Devil (XV)", 
+         "The Tower (XVI)", 
+         "The Star (XVII)", 
+        "The Moon (XVIII)", 
+        "The Sun (XIX)", 
+         "Judgment (XX)", 
+         "The World (XXI)"] 
 
 #draw some cards
 reading = random.sample(cards, 3)
+print(reading)
