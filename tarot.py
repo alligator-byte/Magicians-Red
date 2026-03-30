ef6484fc867e925d4871018fa4a6a792fd0fee25
Luke <23814635@student.uwa.edu.au>
2026-03-30 15:41:25 +0800

    Dictionary & sub-dictionary added
    Each tarot card represented with a default description of each one
    Past, present and future still remain empty for now.

--- tarot.py
+++ tarot.py
@@ -31,18 +31,159 @@ print(reading)
 #dictionary with sub dictionary idea
 interpretation = {
     "The Fool (0)":{
-        "default": "BLANK",
+        "default": "Innocence and spontaneity",
         "past": "BLANK",
         "present":"BLANK",
         "future": "BLANK"
     },
+
     "The Magician (I)":{
-        "default": "BLANK",
+        "default": "New beginnings",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The High Priestess (II)":{
+        "default": "Intuition and arcane knowledge",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Empress (III)":{
+        "default": "Growth of new life",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Emperor (IV)":{
+        "default": "Order and control",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Hierophant (V)":{
+        "default": "Tradition and conformity",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Lovers (VI)":{
+        "default": "Conscious connections and bonds",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Chariot (VII)":{
+        "default": "Invasion and victory",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "Strength (VIII)":{
+        "default": "Resilience, compassion, and confidence",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Hermit (IX)":{
+        "default": "Introspection and contemplation",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Wheel of Fortune (X)":{
+        "default": "Change and unpredictability",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "Justice (XI)":{
+        "default": "Fairness and consequences",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Hanged Man (XII)":{
+        "default": "Different perspectives",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "Death (XIII)":{
+        "default": "Self-awareness and transformation",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "Temperance (XIV)":{
+        "default": "Balance and moderation",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Devil (XV)":{
+        "default": "Confusion and misfortune",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Tower (XVI)":{
+        "default": "Unexpected interruptions and chaos",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Star (XVII)":{
+        "default": "Optimism, discernment, and hope",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Moon (XVIII)":{
+        "default": "Troubled waters, lies, betrayal, and fear of the unknown",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The Sun (XIX)":{
+        "default": "Abundance, happiness, and fun",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+    "Judgment (XX)":{
+        "default": "Rebirth and self-reflection",
+        "past": "BLANK",
+        "present":"BLANK",
+        "future": "BLANK"
+    },
+
+    "The World (XXI)":{
+        "default": "Achievement and a pause before the next cycle",
         "past": "BLANK",
         "present":"BLANK",
         "future": "BLANK"
     }
 }
+
 
 #lists for each - then use the indexes to find
 past = ["Info here...",
